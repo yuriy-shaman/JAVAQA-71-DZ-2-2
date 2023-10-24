afb4aea6f15a2b0454f55a7f0e70220c5a50978f
YuriS <yuri-s@list.ru>
2023-10-24 11:44:23 +0300

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -6,14 +6,14 @@ public class Main {
         int addend = 1100;
         int bonus = 0;
 
-        if ( addend > 1000 ) {
-        bonus = addend / 100;
-    }
+        if (addend > 1000) {
+            bonus = addend / 100;
+        }
 
-       // int bonus = addend > 1000 ? addend / 100 : 0;
+        // int bonus = addend > 1000 ? addend / 100 : 0;
 
 
-    int finalBalance = initionalBalance + addend + bonus;
+        int finalBalance = initionalBalance + addend + bonus;
 
         System.out.println("Ваш итоговый баланс: " + finalBalance);
         System.out.println("Ваши бонусы: " + bonus);
